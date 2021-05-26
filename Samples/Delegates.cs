48dfe98bf804fd06fcc460684567ab9b96b5eb01
MaheshSampath123 <83757151+MaheshSampath123@users.noreply.github.com>
2021-05-26 11:15:40 +0530

    Delegates

---
+++ Samples/Delegates.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Samples
+{
+    public delegate void AddDelegate(int x, int y);
+    public delegate string SayDelegate(string str);
+    class Delegates
+    {
+        public void AddNums(int a,int b)
+        {
+            Console.WriteLine(a+b);
+        }
+        public static string SayHello(string name)
+        {
+            return "Hello" + name;
+        }
+        static void Main(string[] args)
+        {
+            Delegates p = new Delegates();
+            AddDelegate ad = new AddDelegate(p.AddNums);
+            ad(100, 100);
+            SayDelegate sd = new SayDelegate(Delegates.SayHello);
+            string str = sd(" sampath");
+            Console.WriteLine(str);
+            Console.ReadLine();
+        }
+    }
+}
