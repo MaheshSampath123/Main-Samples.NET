d2358b6451da38b8977e1a432ee021b5e57f9fb3
MaheshSampath123 <83757151+MaheshSampath123@users.noreply.github.com>
2021-06-02 19:08:19 +0530

    Dynamic

---
+++ Samples/DynamicUsing.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Samples
+{
+    class DynamicUsing
+    {
+        static void Main(string[] args)
+        {
+            object a = 10;
+            object b = "sampath";
+           
+            var c = 10;
+            var d = "mahesh";
+            Console.WriteLine(c+d);
+            dynamic e = 10;
+            dynamic f = "veeru";
+            Console.WriteLine(e+f);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+
+
+
+            /* int a = 10;
+             object b = true;
+             Console.WriteLine(a.GetType());
+             Console.WriteLine(b.GetType()); */
+            //  object n1 = Console.ReadLine();
+            // Console.WriteLine(n1.GetType());
+            Console.ReadLine();
+        }
+    }
+}
